90c39f8e85228c7da637bae4b893fe5923d03b7a
simentesempre <federico.portoghese@gmail.com>
2020-12-29 15:13:39 +0100

    Fixes adds analytics 2

--- components/SplashPage.js
+++ components/SplashPage.js
@@ -9,7 +9,7 @@ const SplashPage = () => {
         <Head>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-CGMJ6RLDVZ"></script>
             <script>
-                var window.dataLayer = window.dataLayer || []
+                var dataLayer = window.dataLayer || []
                 function gtag(){dataLayer.push(arguments)}
                 gtag('js', new Date())
                 gtag('config', 'G-CGMJ6RLDVZ')
